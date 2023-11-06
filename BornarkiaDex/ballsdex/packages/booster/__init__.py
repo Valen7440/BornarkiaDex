1a5ad59a9a2744a24bf4d0a5146a065d5d2c0375
Auguste "laggron42" Charpentier <laggron42@gmail.com>
2023-11-06 11:37:03 +0100

    move folders

--- BornarkiaDex/ballsdex/packages/booster/__init__.py
+++
@@ -1,9 +0,0 @@
-from typing import TYPE_CHECKING
-
-from ballsdex.packages.booster.cog import Booster
-
-if TYPE_CHECKING:
-    from ballsdex.core.bot import BallsDexBot
-
-async def setup(bot: "BallsDexBot"):
-    await bot.add_cog(Booster(bot))
